718748ad1ed8c552456150e9210ccdefad147050
Harkirat Singh <harkirat96@gmail.com>
2023-12-22 00:32:25 +0530

    git rid of frontend code

--- chat-frontend/src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import MyComponent from './myComponent';
-
-export default function Home() {
-	return (
-		<main className="flex flex-col items-center justify-center min-h-screen">
-			<MyComponent />
-		</main>
-	);
-}
