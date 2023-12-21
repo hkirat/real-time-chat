050015fdac35a83ccb9a97d4148c7f1d3a6015d9
Borhan <borhan.dev@gmail.com>
2023-12-21 23:50:38 +0600

    Added eslint & prettier config

--- src/UserManager.ts
+++ src/UserManager.ts
@@ -2,62 +2,62 @@ import { connection } from "websocket";
 import { OutgoingMessage } from "./messages/outgoingMessages";
 
 interface User {
-    name: string;
-    id: string;
-    conn: connection;
+  name: string;
+  id: string;
+  conn: connection;
 }
 
 interface Room {
-    users: User[]
+  users: User[];
 }
 
 export class UserManager {
-    private rooms: Map<string, Room>;
-    constructor() {
-        this.rooms = new Map<string, Room>()
+  private rooms: Map<string, Room>;
+  constructor() {
+    this.rooms = new Map<string, Room>();
+  }
+
+  addUser(name: string, userId: string, roomId: string, socket: connection) {
+    if (!this.rooms.get(roomId)) {
+      this.rooms.set(roomId, {
+        users: [],
+      });
+    }
+    this.rooms.get(roomId)?.users.push({
+      id: userId,
+      name,
+      conn: socket,
+    });
+  }
+
+  removeUser(roomId: string, userId: string) {
+    const users = this.rooms.get(roomId)?.users;
+    if (users) {
+      users.filter(({ id }) => id !== userId);
+    }
+  }
+
+  getUser(roomId: string, userId: string): User | null {
+    const user = this.rooms.get(roomId)?.users.find(({ id }) => id === userId);
+    return user ?? null;
+  }
+
+  broadcast(roomId: string, userId: string, message: OutgoingMessage) {
+    const user = this.getUser(roomId, userId);
+    if (!user) {
+      console.error("User not found");
+      return;
     }
 
-    addUser(name: string, userId: string, roomId: string, socket: connection) {
-        if (!this.rooms.get(roomId)) {
-            this.rooms.set(roomId, {
-                users: []
-            })
-        }
-        this.rooms.get(roomId)?.users.push({
-            id: userId,
-            name,
-            conn: socket
-        })
+    const room = this.rooms.get(roomId);
+    if (!room) {
+      console.error("Rom rom not found");
+      return;
     }
 
-    removeUser(roomId: string, userId: string) {
-        const users = this.rooms.get(roomId)?.users;
-        if (users) {
-            users.filter(({id}) => id !== userId);
-        }
-    }
-    
-    getUser(roomId: string, userId: string): User | null {
-        const user = this.rooms.get(roomId)?.users.find((({id}) => id === userId));
-        return user ?? null;
-    }
-
-    broadcast(roomId: string, userId: string, message: OutgoingMessage) {
-        const user = this.getUser(roomId, userId);
-        if (!user) {
-            console.error("User not found");
-            return;
-        }
-        
-        const room = this.rooms.get(roomId);
-        if (!room) {
-            console.error("Rom rom not found");
-            return;
-        }
-        
-        room.users.forEach(({conn}) => {
-            console.log("outgoing message " + JSON.stringify(message))
-            conn.sendUTF(JSON.stringify(message))
-        })
-     }
+    room.users.forEach(({ conn }) => {
+      console.log("outgoing message " + JSON.stringify(message));
+      conn.sendUTF(JSON.stringify(message));
+    });
+  }
 }
